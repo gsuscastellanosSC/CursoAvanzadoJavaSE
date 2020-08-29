7dd8443ece7925ec59a7d9d0df3b5ff080f0b90f
sc <jesuscastellanospaez@gmail.com>
2020-08-28 22:48:00 -0500

    Se agrega paquete dao data access object
    Signed-off-by: sc <jesuscastellanospaez@gmail.com>

---
+++ amazonviewer/src/com/sc/amazonviewer/dao/MovieDao.java
@@ -0,0 +1,21 @@
+package com.sc.amazonviewer.dao;
+
+import java.util.ArrayList;
+
+import com.sc.amazonviewer.model.Movie;
+
+public interface MovieDao {
+	
+	default Movie setMovieViewed(Movie movie) {
+		return movie;
+	}
+
+	default ArrayList<Movie> read(){
+		ArrayList <Movie> movies = new ArrayList();
+		return movies;
+	}
+	
+	default boolean getMovieViewed() {
+		return false;
+	}
+}
