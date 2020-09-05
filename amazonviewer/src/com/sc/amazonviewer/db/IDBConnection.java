50636b0e2251f433816e7c485c3bcab86aa3cf7d
sc <jesuscastellanospaez@gmail.com>
2020-09-04 22:41:59 -0500

    Se agrega clase con las entidades de BD
    Signed-off-by: sc <jesuscastellanospaez@gmail.com>

---
+++ amazonviewer/src/com/sc/amazonviewer/db/IDBConnection.java
@@ -0,0 +1,24 @@
+package com.sc.amazonviewer.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import static com.sc.amazonviewer.db.DataBase.*;
+
+public interface IDBConnection {
+	
+	default Connection connectToDB() {
+		Connection connection = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(URL+BD, USER, PASSWORD);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally {
+			
+		}
+		return connection;
+	}
+	
+}
